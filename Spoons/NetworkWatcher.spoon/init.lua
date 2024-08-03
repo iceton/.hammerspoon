d44f8777fe052f7e078afe8b0e9993fd609b1895
iceton <git@145x.com>
2024-08-03 08:07:49 -0700

    remove dns toggle code

--- Spoons/NetworkWatcher.spoon/init.lua
+++ Spoons/NetworkWatcher.spoon/init.lua
@@ -65,23 +65,14 @@ function obj:get_dns_server()
   return hs.execute('scutil --dns | grep "nameserver\\[0\\]" | head -1 | sed "s/.*: \\(.*\\)/\\1/"')
 end
 
--- function obj:toggle_dhcp_dns()
---   obj.use_dhcp_dns = not obj.use_dhcp_dns
---   if obj.use_dhcp_dns then
---     obj:set_dhcp_dns()
---   else
---     obj:set_external_dns()
---   end
---   obj:update_menubar()
--- end
-
 function obj:notify(trace)
   local prev = obj.prev_trace
   
   if trace.loc4 ~= prev.loc4 or trace.loc6 ~= prev.loc6 then
     local alert_text = string.format("4 6: %s %s » %s %s", prev.loc4, prev.loc6, trace.loc4, trace.loc6)
     hs.alert.show(alert_text, nil, nil, 3)
-  elseif trace.colo4 ~= prev.colo4 or trace.colo6 ~= prev.colo6 or not obj:is_ip_similar(prev.ip4, trace.ip4) or not obj:is_ip_similar(prev.ip6, trace.ip6) then
+  --elseif trace.colo4 ~= prev.colo4 or trace.colo6 ~= prev.colo6 or not obj:is_ip_similar(prev.ip4, trace.ip4) or not obj:is_ip_similar(prev.ip6, trace.ip6) then
+elseif not obj:is_ip_similar(prev.ip4, trace.ip4) or not obj:is_ip_similar(prev.ip6, trace.ip6) then
     hs.notify.new({
       title = string.format("%s connection", trace.loc4 or trace.loc6),
       informativeText = string.format("4: %s » %s\n6: %s » %s", prev.colo4, trace.colo4, prev.colo6, trace.colo6),
@@ -89,55 +80,6 @@ function obj:notify(trace)
     }):send()
   end
 end
-
--- function obj:is_external_dns()
---   local found = hs.execute('networksetup -getdnsservers Wi-Fi'):find('1.1.1.1')
---   -- logger.d(found)
---   return found ~= nil
--- end
-
--- function obj:check_external_dns_accessible()
---   -- logger.d('check_external_dns_accessible')
---   --'/usr/bin/dig @1.1.1.1 +retry=0 +short +time=1 google.com; echo $?'
---   hs.task.new(
---     '/usr/bin/dig',
---     function(exit_code, stdout, stderr)
---       local is_external_dns_accessible = exit_code == 0
---       -- logger.d(obj.is_external_dns_accessible)
---       if obj.use_dhcp_dns ~= true then
---         if not is_external_dns_accessible then
---           hs.notify.new({
---             title = 'Switching to DHCP DNS',
---             informativeText = 'External DNS inaccessible',
---             withdrawAfter = 10
---           }):send()
---           obj:toggle_dhcp_dns()
---         elseif obj.use_dhcp_dns == nil then
---           obj:set_external_dns()
---         end
---       end
---     end,
---     { '@1.1.1.1', '+retry=0', '+short', '+time=1', 'google.com' }
---   ):start()
--- end
-
--- function obj:set_dhcp_dns()
---   if obj:is_external_dns() then
---     hs.execute('sudo /usr/local/bin/wifi_dns_dhcp.sh')
---     return true
---   else
---     return false
---   end
--- end
-
--- function obj:set_external_dns(force)
---   if force or not obj:is_external_dns() then
---     hs.execute('sudo /usr/local/bin/wifi_dns_spec.sh')
---     return true
---   else
---     return false
---   end
--- end
 
 function obj:is_network_reachable(reach_status)
   -- logger.d(reach_status)
@@ -192,10 +134,6 @@ function obj:refresh_trace6(trace)
     '/usr/bin/curl',
     function(exit_code, stdout, stderr)
       local trace6 = (exit_code == 0 and obj:trace_to_table(stdout)) or NIL_TRACE
-      -- if not obj:is_ip_similar(trace.ip, obj.prev_trace6.ip) or trace.loc ~= obj.prev_trace6.loc then
-      --   obj:notify(obj.prev_trace4, trace)
-      -- end
-      -- obj.prev_trace6 = trace
       trace.colo6 = trace6.colo
       trace.ip6 = trace6.ip
       trace.loc6 = trace6.loc
