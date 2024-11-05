ada7b9444823a616af9648fbc750d98fec3d5232
iceton <git@145x.com>
2024-11-05 16:41:56 +0900

    inline power watcher

--- Spoons/PowerWatcher.spoon/init.lua
+++ Spoons/PowerWatcher.spoon/init.lua
@@ -3,16 +3,12 @@ local obj = {}
 -- use logger.d("log message")
 local logger = hs.logger.new('PowerWatch', 'debug')
 
-local function is_mute_event(event)
-  return event == hs.caffeinate.watcher.systemWillPowerOff or event == hs.caffeinate.watcher.systemWillSleep
-end
-
 function obj:start()
   obj.watcher = hs.caffeinate.watcher.new(function(event)
-    if hs.audiodevice.defaultOutputDevice():inUse() then
-      return
-    end
-    if is_mute_event(event) then
+    -- logger.d(hs.caffeinate.watcher[event])
+    local is_device_idle = not hs.audiodevice.defaultOutputDevice():inUse()
+    local is_mute_event = event == hs.caffeinate.watcher.screensDidLock
+    if is_device_idle and is_mute_event then
       hs.audiodevice.defaultOutputDevice():setVolume(0)
     end
   end)
